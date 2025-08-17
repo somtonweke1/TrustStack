06bac42eac027d6e9e352e9a389a31ad53a66bf4
Teezy <sonwe1@morgan.edu>
2025-08-17 17:42:34 -0400

    🧹 Remove 'User' text from header top bar

--- client/src/components/Layout.js
+++ client/src/components/Layout.js
@@ -114,7 +114,6 @@ const Layout = ({ children }) => {
                 <div className="w-8 h-8 bg-gradient-to-br from-blue-400 to-blue-600 rounded-full flex items-center justify-center">
                   <User className="h-4 w-4 text-white" />
                 </div>
-                <span className="text-sm font-medium text-gray-700">User</span>
               </div>
             </div>
           </div>
